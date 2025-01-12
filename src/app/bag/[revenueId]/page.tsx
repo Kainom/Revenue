1440174d4c2ef23dd50a10f0083bd5dfa6bba8ba
Kainom <eleuteriokaina@gmail.com>
2025-01-12 15:39:12 -0300

    NotFound page

--- src/app/bag/[revenueId]/page.tsx
+++ src/app/bag/[revenueId]/page.tsx
@@ -6,6 +6,9 @@ type Slug = {
 
 export default async function Revenue({ params }: Slug) {
   const {revenueId} = await params;
+  // const fetch api = getData(revenueId);
+  //if(!fetch)
+  // notFound();
   console.log(params);
   return <h1 className="text-red-200">Revenue {revenueId}</h1>;
 }
