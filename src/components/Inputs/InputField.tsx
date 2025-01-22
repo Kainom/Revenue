77c7d1af199e2ec189d7cf8208e88b91aa2f3c08
Kainom <eleuteriokaina@gmail.com>
2025-01-21 23:46:19 -0300

    I have been added some modifications in calc page

--- src/components/Inputs/InputField.tsx
+++ src/components/Inputs/InputField.tsx
@@ -4,7 +4,7 @@ interface Propertie {
   properties: {
     type: string;
     placeholder: string;
-    width:number
+    width?:string
   };
 }
 
@@ -13,14 +13,14 @@ function padrao({ properties }: Propertie) {
 
   if (!properties.type) properties.type = "text";
 
-  if (!properties.width) properties.width = 200;
+  if (!properties.width) properties.width = "w-3/4";
 }
 export const InputField = ({ properties }: Propertie): ReactElement => {
   padrao({ properties });
 
   return (
     <React.Fragment>
-      <input className="border-none py-1 px-4 rounded-sm my-4 w-2/4" width={properties.width} type={properties.type} placeholder={properties.placeholder} />
+      <input className={`border-none py-1 px-4 rounded-sm  ${properties.width}`}  type={properties.type} placeholder={properties.placeholder} />
     </React.Fragment>
   );
 };
